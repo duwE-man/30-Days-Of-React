3b013da3d0f9351d1bbdcf7f759e2803443b32af
David Koteles <koteles.d25@gmail.com>
2021-12-22 15:33:43 +0100

    user-card

---
+++ 03_Day_Setting_Up/03_setting_up_boilerplate/src/user-card/src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='background'>
+        <div className='icon-image'>
+          <img src="asabeneh.jpg" alt="Programmer " />
+        </div>
+        
+        <div className='name'>
+          Asabeneh Yetayeh
+        </div>
+        
+        <div className='additional-info'>
+          Senior Developer, Finland
+        </div>
+
+        <div className='skills-header'>Skills</div>
+        <div className='skills'>
+          <label className='label'>HTML</label>
+          <label className='label'>CSS</label>
+          <label className='label'>Sass</label>
+          <label className='label'>JS</label>
+          <label className='label'>React</label>
+          <label className='label'>Redux</label>
+          <label className='label'>Node</label>
+          <label className='label'>MongoDB</label>
+          <label className='label'>Python</label>
+          <label className='label'>Flash</label>
+          <label className='label'>Django</label>
+          <label className='label'>Numpy</label>
+          <label className='label'>Pandas</label>
+          <label className='label'>MySQL</label>
+          <label className='label'>GraphQL</label>
+          <label className='label'>D3.js</label>
+          <label className='label'>Gatsby</label>
+          <label className='label'>Docker</label>
+          <label className='label'>Heroku</label>
+          <label className='label'>Git</label>
+        </div>
+
+        <div className='registration'>Joined on Aug 30, 2020</div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
